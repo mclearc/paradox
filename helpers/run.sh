1ea6c4fe7100c21b1d0b3ce9eb193388c446114e
Colin McLear <mclear@fastmail.com>
2022-12-28 23:53:49 -0500

    Revise shell script

--- helpers/run.sh
+++ helpers/run.sh
@@ -1,20 +1,23 @@
 #!/bin/bash -ex
 
+EMACS='/Applications/Emacs.app/Contents/MacOS/Emacs'
+
 if [[ -z "$EMACS" ]]; then
     echo "NO EMACS!!";
     exit 1;
 else
     # Paradox
-    cd ~/.paradox
+    # cd ~/.paradox
+    cd ~/bin/lisp-projects/paradox/
     git fetch origin
     git checkout data &> /dev/null
     git pull &> /dev/null
     git merge origin/master
-    cd ~/.paradox/helpers
+    cd ~/bin/lisp-projects/paradox/helpers
     /usr/bin/nice $EMACS --batch -Q \
-                  -L . -L .. -l paradox-counter.el \
-                  --eval '(setq debug-on-error t)' \
-                  -f paradox-generate-star-count
+           -L . -L .. -l paradox-counter.el \
+           --eval '(setq debug-on-error t)' \
+           -f paradox-generate-star-count
     git add .. &> /dev/null
     git commit -m "$(date)" &> /dev/null
     git push -v origin data:refs/heads/data &> /dev/null;
